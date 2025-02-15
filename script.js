797540cf97cbbc5fedf7d73c8dc6bf419eeca4aa
soyouber <soyouber@outlook.com>
2025-02-15 18:59:56 +0800

    BETTER

--- script.js
+++ script.js
@@ -6,13 +6,22 @@ function toggleSidebar() {
 function showContent(sectionId) {
     const introduction = document.getElementById('introduction');
     const howToUse = document.getElementById('how-to-use');
+    const comments = document.getElementById('comments');
+
     if (sectionId === 'introduction') {
         introduction.style.display = 'block';
         howToUse.style.display = 'none';
+        comments.style.display = 'none';
     } else if (sectionId === 'how-to-use') {
         introduction.style.display = 'none';
         howToUse.style.display = 'block';
+        comments.style.display = 'none';
+    } else if (sectionId === 'comments') {
+        introduction.style.display = 'none';
+        howToUse.style.display = 'none';
+        comments.style.display = 'block';
     }
+
     document.getElementById('sidebar').classList.remove('active');
 }
 
